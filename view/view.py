8226e3ce0f3fafa898e6171072568a4084bae8fc
Christophe <87915060+fox7725@users.noreply.github.com>
2023-03-30 12:29:33 +0200

    Petite inversion des fichiers corrigée

--- view/view.py
+++ view/view.py
@@ -51,17 +51,3 @@ class FormulaireNouveauJoueur :
 
 class FormulaireRechercheJoueur :
     pass
-
-if __name__ == "__main__":
-    print("="*29)
-    print(" ")
-    print("BIENVENUE DANS VOTRE LOGICIEL")
-    print("DE GESTION DE TOURNOIS DE")
-    print("PING-PONG")
-    print(" ")
-    print("="*29)
-    input("Pressez 'ENTER' pour continuer")
-
-    menu = MenuPrincipal()
-    menu.menuprincipal()
-    choix = menu.code_menu_principal
